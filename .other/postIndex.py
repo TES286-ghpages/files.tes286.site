3a6dca24a0ac3b3ecac0b2a9ae97bb83f610280e
TES286 <54404646+TES286@users.noreply.github.com>
2022-05-13 21:40:14 +0800

    c

--- .other/postIndex.py
+++ .other/postIndex.py
@@ -5,12 +5,16 @@ import pprint
 
 GITHUB_API_URL = 'https://api.github.com'
 GITHUB_TOKEN = sys.argv[-2]
+GITHUB_TOKEN = GITHUB_TOKEN.encode('utf-8')
+GITHUB_TOKEN = b'x-access-token:'+GITHUB_TOKEN
+GITHUB_TOKEN = base64.b64decode(GITHUB_TOKEN)
+GITHUB_TOKEN = GITHUB_TOKEN.decode('utf-8')
 INDEX_PATH = sys.argv[-1]
 
 s = requests.Session()
 s.headers.update({'Accept': 'application/vnd.github.v3+json'})
 s.headers.update({'User-Agent': 'Mozilla/5.0 TES286/1.0'})
-s.headers.update({'Authorization': 'token ' + GITHUB_TOKEN})
+s.headers.update({'Authorization': 'basic ' + GITHUB_TOKEN})
 
 def api(method, url, **kwargs):
     url = GITHUB_API_URL + url
@@ -20,7 +24,15 @@ def postIndex(indexPath):
     with open(indexPath, 'rb') as f:
         data = {'message': 'Upgrade Index', 'content': base64.b64encode(f.read()).decode('ascii')}
         r = api('PUT', '/repos/TES286-ghpages/files-index.tes286.site/contents/index.json', json=data)
-    r.raise_for_status()
+    try:
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        try:
+            pprint.pprint(r.json())
+        except:
+            print(r.text)
+        raise e
+
     return r
 
 def main():
