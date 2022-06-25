e7ee29420a30ddd20beae4fd3b848cc76cd354d5
TES286 <54404646+TES286@users.noreply.github.com>
2022-06-25 13:25:10 +0800

    Delete sh directory

--- sh/install/tmcl/laster/tmcl.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/TES286/TMCL.git TMCL && cd TMCL
-python3.8 -m pip install -r requirements.txt
-pyinstaller -F run.py
-cp dist/run /usr/bin/tmcl
-
-echo "You can use command \"TMCL\" to run TMCL"
